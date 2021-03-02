deee6f7350554ef97734a234741c781403b9d474
Imari Pickens <imaridp23@gmail.com>
2021-03-01 22:00:42 -0500

    Adding comments

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,33 +1,48 @@
+// Setting variable for database
 var db;
+
+// Creating request to open budget database
 var request = indexedDB.open('budget', 1);
 
+// Request for error event in case of error(s)
 request.onerror = function (event) {
     console.log("ERROR! " + event.target.errorCode);
 };
 
+// Request for success event if no errors
 request.onsuccess = function (event) {
     db = event.target.result;
+    // checking if app is online before reading from database
     if (navigator.onLine) {
         checkDatabase();
     }
 };
 
+// Creating object store for database called "pending" and set autoIncrement to true
 request.onupgradeneeded = function (event) {
     var db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
 
+// Creating function to save record to database
 function saveRecord(record) {
+    // creates a transaction on the pending database using readwrite access
     var transaction = db.transaction(["pending"], "readwrite");
 
+    // accesses pending object store
     var store = transaction.objectStore("pending");
 
+    // adds records to store
     store.add(record);
 }
 
+// Creating function to check database 
 function checkDatabase() {
+    // opens a transaction on the pending database
     var transaction = db.transaction(["pending"], "readwrite");
+    // accesses the pending object store
     var store = transaction.objectStore("pending");
+    // getting all records from store and setting as a variable
     var getAll = store.getAll();
 
     getAll.onsuccess = function () {
@@ -42,12 +57,18 @@ function checkDatabase() {
             })
                 .then(response => response.json())
                 .then(() => {
+                    // upon success, opens a transaction on the pending database
                     var transaction = db.transaction(["pending"], "readwrite");
+
+                    // accesses the pending object store
                     var store = transaction.objectStore("pending");
+
+                    // clears all records in store
                     store.clear();
                 });
         }
     };
 }
 
+// eventlistener for when app comes online
 window.addEventListener("online", checkDatabase);
